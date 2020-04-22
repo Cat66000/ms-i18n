312f204e42f987cf82541d700907d149b9d03237
Cat66000 <yoanguardiola@outlook.com>
2020-04-22 18:27:58 +0200

    Put the whole index.js in a class - Convert the whole code to the latest EcmaScript (index.js & tests.js) - Upgrade the devDep to their latest update

--- index.js
+++ index.js
@@ -1,108 +1,129 @@
 /**
- * Helpers.
+ * Symbols.
  */
+const _plural = Symbol("plural");
+const _parse = Symbol("parse");
+const _fmtShort = Symbol("fmtShort");
+const _fmtLong = Symbol("fmtLong");
 
-var s = 1000;
-var m = s * 60;
-var h = m * 60;
-var d = h * 24;
-var w = d * 7;
-var y = d * 365.25;
+class MS {
 
-/**
- * Parse or format the given `val`.
- *
- * Options:
- *
- *  - `long` verbose formatting [false]
- *
- * @param {String|Number} val
- * @param {Object} [options]
- * @throws {Error} throw an error if val is not a non-empty string or a number
- * @return {String|Number}
- * @api public
- */
+  constructor(lang = "en") {
+    // this.lang = require(`./langs/${lang}.json`);
 
-module.exports = function(val, options) {
-  options = options || {};
-  var type = typeof val;
-  if (type === 'string' && val.length > 0) {
-    return parse(val);
-  } else if (type === 'number' && isFinite(val)) {
-    return options.long ? fmtLong(val) : fmtShort(val);
+    /**
+     * Helpers.
+     */
+
+    this.s = 1000;
+    this.m = this.s * 60;
+    this.h = this.m * 60;
+    this.d = this.h * 24;
+    this.w = this.d * 7;
+    this.y = this.d * 365.25;
   }
-  throw new Error(
-    'val is not a non-empty string or a valid number. val=' +
-      JSON.stringify(val)
-  );
-};
 
-/**
- * Parse the given `str` and return milliseconds.
- *
- * @param {String} str
- * @return {Number}
- * @api private
- */
+  /**
+   * Parse or format the given `val`.
+   *
+   * Options:
+   *
+   *  - `long` verbose formatting [false]
+   *
+   * @param {String|Number} val
+   * @param {Object} [options]
+   * @throws {Error} throw an error if val is not a non-empty string or a number
+   * @return {String|Number}
+   * @api public
+   */
 
-function parse(str) {
-  str = String(str);
-  if (str.length > 100) {
-    return;
+  format(val, options) {
+    options = options || {};
+    const type = typeof val;
+    if (type === 'string' && val.length > 0) {
+      return this[_parse](val);
+    } else if (type === 'number' && isFinite(val)) {
+      return options.long ? this[_fmtLong](val) : this[_fmtShort](val);
+    }
+    throw new Error(
+      `val is not a non-empty string or a valid number. val=${
+      JSON.stringify(val)}`
+    );
   }
-  var match = /^(-?(?:\d+)?\.?\d+) *(milliseconds?|msecs?|ms|seconds?|secs?|s|minutes?|mins?|m|hours?|hrs?|h|days?|d|weeks?|w|years?|yrs?|y)?$/i.exec(
-    str
-  );
-  if (!match) {
-    return;
-  }
-  var n = parseFloat(match[1]);
-  var type = (match[2] || 'ms').toLowerCase();
-  switch (type) {
-    case 'years':
-    case 'year':
-    case 'yrs':
-    case 'yr':
-    case 'y':
-      return n * y;
-    case 'weeks':
-    case 'week':
-    case 'w':
-      return n * w;
-    case 'days':
-    case 'day':
-    case 'd':
-      return n * d;
-    case 'hours':
-    case 'hour':
-    case 'hrs':
-    case 'hr':
-    case 'h':
-      return n * h;
-    case 'minutes':
-    case 'minute':
-    case 'mins':
-    case 'min':
-    case 'm':
-      return n * m;
-    case 'seconds':
-    case 'second':
-    case 'secs':
-    case 'sec':
-    case 's':
-      return n * s;
-    case 'milliseconds':
-    case 'millisecond':
-    case 'msecs':
-    case 'msec':
-    case 'ms':
-      return n;
-    default:
-      return undefined;
-  }
-}
+  /**
+   * Pluralization helper.
+   */
 
-/**
+  [_plural](ms, msAbs, n, name) {
+    const isPlural = msAbs >= n * 1.5;
+    return `${Math.round(ms / n)} ${name}${isPlural ? 's' : ''}`;
+  }
+
+  /**
+   * Parse the given `str` and return milliseconds.
+   *
+   * @param {String} str
+   * @return {Number}
+   * @api private
+   */
+
+  [_parse](str) {
+    str = String(str);
+    if (str.length > 100) {
+      return;
+    }
+    const match = /^(-?(?:\d+)?\.?\d+) *(milliseconds?|msecs?|ms|seconds?|secs?|s|minutes?|mins?|m|hours?|hrs?|h|days?|d|weeks?|w|years?|yrs?|y)?$/i.exec(
+      str
+    );
+    if (!match) {
+      return;
+    }
+    const n = parseFloat(match[1]);
+    const type = (match[2] || 'ms').toLowerCase();
+    switch (type) {
+      case 'years':
+      case 'year':
+      case 'yrs':
+      case 'yr':
+      case 'y':
+        return n * this.y;
+      case 'weeks':
+      case 'week':
+      case 'w':
+        return n * this.w;
+      case 'days':
+      case 'day':
+      case 'd':
+        return n * this.d;
+      case 'hours':
+      case 'hour':
+      case 'hrs':
+      case 'hr':
+      case 'h':
+        return n * this.h;
+      case 'minutes':
+      case 'minute':
+      case 'mins':
+      case 'min':
+      case 'm':
+        return n * this.m;
+      case 'seconds':
+      case 'second':
+      case 'secs':
+      case 'sec':
+      case 's':
+        return n * this.s;
+      case 'milliseconds':
+      case 'millisecond':
+      case 'msecs':
+      case 'msec':
+      case 'ms':
+        return n;
+      default:
+        return undefined;
+    }
+  }
+  /**
  * Short format for `ms`.
  *
  * @param {Number} ms
@@ -110,53 +131,48 @@ function parse(str) {
  * @api private
  */
 
-function fmtShort(ms) {
-  var msAbs = Math.abs(ms);
-  if (msAbs >= d) {
-    return Math.round(ms / d) + 'd';
+  [_fmtShort](ms) {
+    const msAbs = Math.abs(ms);
+    if (msAbs >= this.d) {
+      return `${Math.round(ms / this.d)}d`;
+    }
+    if (msAbs >= this.h) {
+      return `${Math.round(ms / this.h)}h`;
+    }
+    if (msAbs >= this.m) {
+      return `${Math.round(ms / this.m)}m`;
+    }
+    if (msAbs >= this.s) {
+      return `${Math.round(ms / this.s)}s`;
+    }
+    return `${ms}ms`;
   }
-  if (msAbs >= h) {
-    return Math.round(ms / h) + 'h';
+
+  /**
+   * Long format for `ms`.
+   *
+   * @param {Number} ms
+   * @return {String}
+   * @api private
+   */
+
+  [_fmtLong](ms) {
+    const msAbs = Math.abs(ms);
+    if (msAbs >= this.d) {
+      return this[_plural](ms, msAbs, this.d, 'day');
+    }
+    if (msAbs >= this.h) {
+      return this[_plural](ms, msAbs, this.h, 'hour');
+    }
+    if (msAbs >= this.m) {
+      return this[_plural](ms, msAbs, this.m, 'minute');
+    }
+    if (msAbs >= this.s) {
+      return this[_plural](ms, msAbs, this.s, 'second');
+    }
+    return `${ms} ms`;
   }
-  if (msAbs >= m) {
-    return Math.round(ms / m) + 'm';
-  }
-  if (msAbs >= s) {
-    return Math.round(ms / s) + 's';
-  }
-  return ms + 'ms';
+
 }
 
-/**
- * Long format for `ms`.
- *
- * @param {Number} ms
- * @return {String}
- * @api private
- */
-
-function fmtLong(ms) {
-  var msAbs = Math.abs(ms);
-  if (msAbs >= d) {
-    return plural(ms, msAbs, d, 'day');
-  }
-  if (msAbs >= h) {
-    return plural(ms, msAbs, h, 'hour');
-  }
-  if (msAbs >= m) {
-    return plural(ms, msAbs, m, 'minute');
-  }
-  if (msAbs >= s) {
-    return plural(ms, msAbs, s, 'second');
-  }
-  return ms + ' ms';
-}
-
-/**
- * Pluralization helper.
- */
-
-function plural(ms, msAbs, n, name) {
-  var isPlural = msAbs >= n * 1.5;
-  return Math.round(ms / n) + ' ' + name + (isPlural ? 's' : '');
-}
+module.exports = MS;
